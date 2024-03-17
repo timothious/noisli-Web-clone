20e70e0efeae4d7d4488b39d092de5f62526be4f
mrttrack <251683950@formanite.fccollege.edu.pk>
2024-03-17 22:39:56 +0500

    Noisli Java Script File

---
+++ script.js
@@ -0,0 +1,7 @@
+
+document.addEventListener("DOMContentLoaded", function() {
+    var signupButton = document.getElementById("signupButton");
+    signupButton.addEventListener("click", function() {
+        window.location.href = "sighnin.html"; // Replace "signup.html" with your actual signup page URL
+    });
+});
